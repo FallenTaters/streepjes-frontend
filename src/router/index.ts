5e8986321c29601c6910d5bf05bb1d9c3f0dedcf
marty <m.karkossa@ultraware.nl>
2021-05-12 16:44:37 +0200

    add navigation guards

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,33 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router"
+import { Role } from "@/type/user"
+import {
+    createRouter,
+    createWebHistory,
+    NavigationGuard,
+    RouteRecordRaw,
+} from "vue-router"
+import { store } from "@/store/index"
+
+const authGuard: NavigationGuard = function (to, from, next) {
+    const role = store.getters.role
+    if (!role || role == Role.NotAuthorized) {
+        next({ name: "login" })
+    }
+}
+
+function roleGuard(role: string): NavigationGuard {
+    return function (to, from, next) {
+        const actual = store.getters.role
+        if (!actual || actual != role) {
+            next({ name: "login" })
+        }
+    }
+}
 
 const routes: Array<RouteRecordRaw> = [
+    {
+        path: "/:pathMatch(.*)*",
+        redirect: "/",
+    },
     {
         path: "/login",
         name: "login",
@@ -10,20 +37,19 @@ const routes: Array<RouteRecordRaw> = [
         path: "/",
         name: "order",
         component: () => import("../views/Order.vue"),
-    },
-    {
-        path: "/:pathMatch(.*)*",
-        redirect: "/",
+        beforeEnter: authGuard,
     },
     {
         path: "/history",
         name: "history",
         component: () => import("../views/History.vue"),
+        beforeEnter: roleGuard(Role.Bartender),
     },
     {
         path: "/billing",
         name: "billing",
         component: () => import("../views/Billing.vue"),
+        beforeEnter: roleGuard(Role.Admin),
     },
 ]
 
