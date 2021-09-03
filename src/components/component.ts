1e1369eec78173e56ce495c6f13dae7da0acc3b0
PotatoesFall <markarkossa@gmail.com>
2021-09-03 15:16:31 +0200

    remove linter errors

--- src/components/component.ts
+++ src/components/component.ts
@@ -1,6 +1,8 @@
 import { defineComponent } from "vue"
 
-export default function (f) {
+export default function (
+    f: () => Record<string, unknown>
+): ReturnType<typeof defineComponent> {
     return defineComponent({
         setup: f,
     })
