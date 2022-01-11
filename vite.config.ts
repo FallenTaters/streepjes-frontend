9256f09c9fb5c2a1d154aabd5d8ce34045df0a9e
Marty <m.karkossa@ultraware.nl>
2022-01-11 12:59:57 +0100

    WIP

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import path from "path"
 import eslintPlugin from "vite-plugin-eslint"
 
 export default defineConfig({
-    plugins: [vue(), eslintPlugin()],
+    plugins: [vue()], //eslintPlugin()],
     resolve: {
         alias: {
-            "@": path.resolve(__dirname, "./src"),
+            "@/": path.resolve(__dirname, "./src"),
         },
     },
 })
