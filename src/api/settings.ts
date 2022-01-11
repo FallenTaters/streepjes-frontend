ccd96d036f18a210fb3078fe2f763a7fcabb8003
Marty <m.karkossa@ultraware.nl>
2022-01-11 14:01:34 +0100

    WIP

--- src/api/settings.ts
+++ src/api/settings.ts
@@ -1,4 +1,4 @@
 let endpoint = `http://localhost:8081`
-if (import.meta.env.VUE_APP_ENDPOINT) endpoint = import.meta.env.VUE_APP_ENDPOINT
+if (import.meta.env.VUE_APP_ENDPOINT) endpoint = import.meta.env.VUE_APP_ENDPOINT as string
 
 export const apiEndpoint = endpoint
